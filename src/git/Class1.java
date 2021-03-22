edb48468997e172292817a64f7edf2bb06ed7a24
N2463329272 <2463329272@qq.com>
2021-03-22 15:23:58 +0800

    这个是我第一次利用Eclipse连接git，上传helloWord文件到我的新仓库里面

---
+++ src/git/Class1.java
@@ -0,0 +1,9 @@
+package git;
+
+public class Class1 {
+	String aa = "123";
+	Class1(String aa){
+		 this.aa = aa;
+		  
+	}
+}
---
+++ src/git/HelloWord.java
@@ -0,0 +1,9 @@
+package git;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		String a = "huige";
+		System.out.println("Hello!!!"+a);
+	}
+}
